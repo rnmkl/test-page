ed6b43c70ad0bcac267d5078a2a40cf26b67bad8
rnmkl <97894757+rnmkl@users.noreply.github.com>
2022-08-17 13:03:10 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+    function getCode(mob) {
+      console.log('mob=' + mob)
+    }
+    function checkCode(fill, code) {
+      console.log('fill=' + fill)
+       console.log('code=' + code)
+    }
