9b84a00595ae6ca6106af1e2233aa405a0a0336e
rnmkl <97894757+rnmkl@users.noreply.github.com>
2022-08-18 14:20:40 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,9 +4,11 @@
 
 function getCode(mob) {
     console.log('mob=' + mob)
+    alert('mob=' + mob)
 }
 
 function checkCode(fill, code) {
     console.log('fill=' + fill)
     console.log('code=' + code)
+    alert('fill=' + fill + '\ncode=' + code)
 }
