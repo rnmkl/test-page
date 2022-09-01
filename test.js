fc3bcd8edd1c8bcc4525ca0d3788ac253cde7d85
rnmkl <97894757+rnmkl@users.noreply.github.com>
2022-09-01 11:41:56 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,11 +4,9 @@
 
 function msisdnClick(mob) {
     console.log('mob=' + mob)
-    alert('mob=' + mob)
 }
 
 function codeClick(fill, code) {
     console.log('fill=' + fill)
     console.log('code=' + code)
-    alert('fill=' + fill + '\ncode=' + code)
 }
