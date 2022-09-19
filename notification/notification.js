7312fa0e22be5e6cf1021dda80b30da0e57777f1
rnmkl <97894757+rnmkl@users.noreply.github.com>
2022-09-19 16:00:49 +0300

    Update notification.js

--- notification/notification.js
+++ notification/notification.js
@@ -2,7 +2,11 @@ class Notification {
   constructor(name, options) {
     this.name = name;
     this.options = options
-    console.log(this)
+    console.log('name=', name);
+    console.log('opt =', options);
     Android.pass(this);
+  }
+  requestPermission() {
+    return "granted";
   }
 }
